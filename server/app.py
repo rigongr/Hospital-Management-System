9e0567086267a7c3c6f6e3fbde6fc283901502c6
rigongr <grapcirigon@gmail.com>
2021-06-15 21:29:27 +0200

    Connected to Mongo Database, sent data successfully to Vue, implemented a for loop in Vue aswell

--- server/app.py
+++ server/app.py
@@ -1,11 +1,12 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
+from flask_pymongo import PyMongo
+
 from hms import create_app
 
 
-print(create_app)
-
 app = create_app()
+
 # configuration
 DEBUG = True
 
--- server/hms/users/routes.py
+++ server/hms/users/routes.py
@@ -1,10 +1,19 @@
 from flask import Flask, jsonify, Blueprint
 from flask_cors import CORS
+from hms import mongo
 
 
 users = Blueprint('users', __name__)
 
+users_collection = mongo.db.listingsAndReviews
+
 # sanity check route
 @users.route('/ping', methods=['GET'])
 def ping_pong():
-    return jsonify('PONG! This is coming from the backend (Python, Flask).')
+    air_bnb_data = mongo.db.listingsAndReviews.find()[2]
+    keys = []
+    for key in air_bnb_data:
+        keys.append(key)
+        
+    print(keys)
+    return jsonify(keys)
