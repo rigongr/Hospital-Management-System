4449a6740b3264e384ee7e545652ec034c4c883f
rigongr <grapcirigon@gmail.com>
2021-09-08 21:17:38 +0200

    feat: tested post method, it works

--- server/hms/users/routes.py
+++ server/hms/users/routes.py
@@ -1,6 +1,7 @@
-from typing import List
+from os import write
+from typing import List, Optional
 
-from flask import jsonify, Blueprint
+from flask import jsonify, Blueprint, request
 from flask_login import login_user, current_user, logout_user, login_required
 
 from hms import client
@@ -15,13 +16,15 @@ db = client.hms_db
 users_collection = db.users
 
 
-@users.route('/ping', methods=['GET'])
+@users.route('/ping', methods=['GET', 'POST'])
 def ping_pong():
-    
+    if request.method == 'POST':
+        write_log('POST METHOD')
+        write_log(request.get_json())
+        
     doc = Doctor("Doc1", "Filan Fisteku", 38349700700, "ff@gmail.com", "bajram curri", "filan123", "Heart Surgeon")
 
     doc.register("Doc1", "Filan Fisteku", 38349700700, "ff@gmail.com", "bajram curri", "filan123", "Heart Surgeon")
     
     login_user(doc)
-
     return doc.json()
