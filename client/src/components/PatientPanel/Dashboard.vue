e8fdf95e75781614a51ea245d4d5f84080a585ce
Korab Qorri <korabqorri1@gmail.com>
2021-09-22 23:13:37 +0200

    Added components for admin, doctor and patient dashboards

--- client/src/components/PatientPanel/Dashboard.vue
+++ client/src/components/PatientPanel/Dashboard.vue
@@ -1,1 +1,69 @@
-    
+<template>
+  <sidebar-menu :menu="menu" />
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      menu: [
+        {
+          header: true,
+          title: "Main Navigation",
+          hiddenOnCollapse: true
+        },
+        {
+          href: "/patient/dashboard",
+          title: "Dashboard",
+          icon: "fa fa-user"
+        },
+        {
+          href: "/patient",
+          title: "Patient",
+          icon: "fa fa-chart-area",
+          child: [
+            {
+              href: "/patient/departments",
+              title: "Departments"
+            },
+            {
+              href: "/patient/doctors",
+              title: "Doctors"
+            },
+            {
+              href: "/patient/appointments",
+              title: "Appointments"
+            },
+            {
+              href: "/patient/recipes",
+              title: "Recipes"
+            },
+            {
+              href: "/patient/drugs",
+              title: "DrugStore"
+            }
+          ]
+        },
+        {
+          href: "/doctor",
+          title: "Tools",
+          icon: "fa fa-user"
+        },
+        {
+          href: "/doctor",
+          title: "Reports",
+          icon: "fa fa-chart-area"
+        },
+        {
+          href: "/patient",
+          title: "Sign Out",
+          icon: "fa fa-user"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style scoped>
+</style>
---
+++ client/src/components/PatientPanel/Details/DoctorsManagementDetails.vue
@@ -0,0 +1,94 @@
+<template>
+  <div>
+    <NewHeader />
+    <PatientSideBar />
+    <b-card-group deck style="margin: 20px 50px 0 400px">
+      <b-card header="Doctors">
+        <b-list-group v-for="item in data" :key="item.id">
+          <b-list-group-item
+            :to="{ name: 'service-management-details', params: { prodId: item.id } }"
+          >
+            {{item.name}}
+            <b-button
+              size="sm"
+              @click="removeService(item)"
+              :to="{ name: 'service-management'}"
+              class="float-right"
+              variant="success"
+            >Appoint</b-button>
+          </b-list-group-item>
+        </b-list-group>
+        <!-- <b-button size="sm" class="float-left" variant="success" :to="{ name: 'add-service'}">Create</b-button> -->
+
+        <p class="card-text mt-2">
+          Quis magna Lorem anim amet ipsum do mollit sit cillum voluptate ex nulla tempor. Laborum
+          consequat non elit enim exercitation cillum aliqua consequat id aliqua. Esse ex consectetur
+          mollit voluptate est in duis laboris ad sit ipsum anim Lorem.
+        </p>
+      </b-card>
+    </b-card-group>
+  </div>
+</template>
+
+<script>
+import NewHeader from "../../layouts/NewHeader";
+import PatientSideBar from "../../layouts/PatientSideBar.vue";
+// import { mapGetters, mapActions } from "vuex";
+// import axios from "axios";
+
+export default {
+  name: "Dashboard",
+  data() {
+    return {
+      data: [{
+      id: 'asd123',
+      name: 'Doktorri 1',
+      description: 'Departamenti per Kirurgji'
+    },
+    {
+      id: 'asd321',
+      name: 'Doktorri 2',
+      description: 'Departamenti per Stomatologji'
+    }]
+      // total: {},
+      // unassigned: {},
+      // overdue: {},
+      // closed: {},
+      // my: 1
+    };
+  },
+  methods: {
+    // ...mapActions(["getAllServices"])
+  },
+  // computed: mapGetters(["allServices"]),
+  mounted() {
+    // this.getAllServices();
+    // axios.get("http://localhost:5000/api/services").then(response => {
+    //   this.total = response.data.length;
+    //   let countUn = 0;
+    //   let countOver = 0;
+    //   let countClose = 0;
+    //   response.data.forEach(el => {
+    //     if (el.unassigned === true) {
+    //       countUn++;
+    //     }
+    //     if (el.overdue === true) {
+    //       countOver++;
+    //     }
+    //     if (el.closed === true) {
+    //       countClose++;
+    //     }
+    //   });
+    //   this.unassigned = countUn;
+    //   this.overdue = countOver;
+    //   this.closed = countClose;
+    // });
+  },
+  components: {
+    NewHeader,
+    PatientSideBar
+  }
+};
+</script>
+
+<style scoped></style>
